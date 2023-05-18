2ff033d33ff6157de6451bdb48f51e1dc26f36d2
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2023-05-18 19:56:07 +0200

    Add throttling to crawler

--- tmdb_crawler.py
+++ tmdb_crawler.py
@@ -1,5 +1,6 @@
 import argparse
 import gzip
+import time
 
 parser = argparse.ArgumentParser(
     prog="TMDB Crawler",
@@ -20,8 +21,19 @@ args = parser.parse_args()
 
 print("-- Starting TMDB crawler --")
 
+MIN_WAIT = 1.0 / 40.0   # max of 40 requests per second
+last_request_instant = time.time()
 with gzip.open(args.file, "rt") as file:
     for line in file:
+        ellapsed = time.time() - last_request_instant
+        if ellapsed < MIN_WAIT:
+            time.sleep(MIN_WAIT - ellapsed)
+        last_request_instant = time.time()
+
         id = line[20:].split(",", 1)[0]
-        print(id)
+        
+        print(f"Requesting details of {id}")
+        
+            
+            
 
