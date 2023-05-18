588f452f28a4bbbfbdabf1f43ecf6d2b5e6a740e
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2023-05-18 23:32:44 +0200

    Add compression features to the crawler

--- README.md
+++ README.md
@@ -17,36 +17,45 @@ curl -XGET http://files.tmdb.org/p/exports/movie_ids_05_15_2023.json.gz -o data/
 And then use this file with the utility `tmdb_crawler` that we are providing to retrieve the details of all those movies to get a complete dataset. To do this you can run:
 
 ```sh
-python tmdb_crawler.py -t TMDB_API_TOKEN ./data/movie_ids.json.gz
+python tmdb_crawler.py -z -t TMDB_API_TOKEN ./data/movie_ids.json.gz
 ```
 
-The `tmdb_crawler` is a pretty naïve script built on assumptions over the files provided by TMDB, it sacrifies robustness for speed (the ID are read using substrings, not JSON parsing). You can provide a different file with a subset of movies if you want, but the format must be the same and it must be compressed with `gunzip`.
+This will generate a new file `./data/tmdb_movie_ids,json.gz` with all the populated data. If you want the raw output instead of the compressed file you can omit the `-z` flag, or can unzip the content youself.
+
+Taking into account that the `tmdb_crawler` is a pretty naïve script built on assumptions over the files provided by TMDB, it sacrifies robustness for speed (the ID are read using substrings, not JSON parsing). You can provide a different file with a subset of movies if you want, but the format must be the same and it must be compressed in a way that `gzip` can read it.
 
 For example, if you want to use only the first fifty movies you can do:
 
 ```sh
 gzip -d data/movie_ids.json.gz
 cat data/movie_ids.json | head -n 50 > movie_ids_small.json
-gzip movie_ids_small.json
+gzip data/movie_ids_small.json
 python tmdb_crawler.py -t TMDB_API_TOKEN ./data/movie_ids_small.json.gz
 ```
 
+### Launching the services
+
+TODO. if it's only a single `docker-compose up` it would be perfect.
+
+### Building the index
+
+TODO. curl the index, explain what happens
 
 ## Next steps
 - [x] Download movie dataset
-- [ ] Create crawler service 
+- [x] Create crawler service 
   - [x] Read compressed file
   - [x] Extract IDs
   - [x] Request details
-  - [ ] Append content to file
-  - [ ] Compress output file
-  - [ ] [Optional] Accept output path and name 
-- [ ] Improve embedding module
-  - [ ] Read the docs from Kafka topic "docs"
-  - [ ] Send the embedded docs to Kafka topic "embedded"
+  - [x] Append content to file
+  - [x] Compress output file
+  - [x] [Optional] Accept output path and name 
 - [ ] Create index service
   - [ ] POST /index -> docs
   - [ ] Kafka producer to "docs" topic
+- [ ] Improve embedding module
+  - [ ] Read the docs from Kafka topic "docs"
+  - [ ] Send the embedded docs to Kafka topic "embedded"
 - [ ] Create embedded consumer
   - [ ] Kafka consumer of "embedded" topic
   - [ ] Index the data into elastic/solr/opensearch whatever
--- tmdb_crawler.py
+++ tmdb_crawler.py
@@ -6,97 +6,105 @@ import threading
 import time
 
 parser = argparse.ArgumentParser(
-    prog="TMDB Crawler",
-    description="Tool to automatically query TMDB API /movies to populate with data the provided list"
+    prog='TMDB Crawler',
+    description='Tool to automatically query TMDB API /movies to populate with data the provided list'
 )
 
-parser.add_argument("-t", "--token",
-                    help="Your TMDB API token",
+parser.add_argument('-t', '--token',
+                    help='Your TMDB API token',
                     default=None,
                     required=True)
 
-parser.add_argument("-o", "--output",
-                    help="Path and name to the output file",
+parser.add_argument('-o', '--output',
+                    help='Path and name to the output file',
                     default=None)
 
-parser.add_argument("file",
-                    help="jsonl compressed file with the list of movies to populate",
+parser.add_argument('-z', '--zip',
+                    help='Compresses the output file',
+                    action='store_true',
+                    default=False)
+
+parser.add_argument('file',
+                    help='jsonl compressed file with the list of movies to populate',
                     default=None)
 
 args = parser.parse_args()
 
 def request_tmdb(url, token=args.token):
     return requests.get(url, headers={
-        "accept": "application/json",
-        "Authorization": f"Bearer {token}"
+        'accept': 'application/json',
+        'Authorization': f'Bearer {token}'
     })
 
 
 failed_requests = {}
-
-
 def request_movie_details(movie_id):
     response = request_tmdb(
-        f"https://api.themoviedb.org/3/movie/{movie_id}?language=en-US")
+        f'https://api.themoviedb.org/3/movie/{movie_id}?language=en-US')
     if response.status_code != 200:
         failed_requests[movie_id] = response.text
         return
     return response.text
 
 
-print("> Starting TMDB crawler")
+print('> Starting TMDB crawler')
 
-print(">> Authenticating to TMDB API")
-authResponse = request_tmdb("https://api.themoviedb.org/3/authentication")
+
+print('>> Authenticating to TMDB API')
+authResponse = request_tmdb('https://api.themoviedb.org/3/authentication')
 assert (authResponse.status_code == 200)
-print(">> Authentication successful")
+print('>> Authentication successful')
 
 
-print(">> Launching status update thread")
+print('>> Launching status update thread')
 start_time = time.time()
 counter = 0
-
-
 def print_status():
     while True:
         time.sleep(5)
         ellapsed = time.time() - start_time
         print(
-            f">>> Requested: {counter} movies. Time ellapsed: {ellapsed:.0f} s")
-
+            f'>>> Requested: {counter} movies. Time ellapsed: {ellapsed:.0f} s')
 threading._start_new_thread(print_status, ())
+
+
+print('>> Preparing output file')
+output_path = args.output
+if output_path == None:
+    _, filename = os.path.split(args.file)
+    filename = filename if args.zip else filename[:-3]
+    output_path = f'./data/tmdb_{filename[:-3]}'
+output_file = gzip.open(output_path, 'wb') if args.zip else open(output_path, 'w')
+
+process = lambda a : a + '\n'
+if args.zip:
+    process = lambda a: bytes(a + '\n', 'utf-8') # gzip writting requires bytes
 
 
 MIN_WAIT = 1.0 / 40.0   # max of 40 requests per second
 last_request_instant = start_time
 
-_, filename = os.path.split(args.file)
-output_filetext_path = args.output if args.output != None else f"./data/tmdb_{filename[:-3]}"
-output_file = open(output_filetext_path, "w")
 
-print(">> Requesting details of movies in the input file")
-with gzip.open(args.file, "rt") as file:
+print('>> Requesting details of movies in the input file')
+with gzip.open(args.file, 'rt') as file, output_file:
     for line in file:
         ellapsed = time.time() - last_request_instant
         if ellapsed < MIN_WAIT:
             time.sleep(MIN_WAIT - ellapsed)
         last_request_instant = time.time()
 
-        id = line[20:].split(",", 1)[0]
+        id = line[20:].split(',', 1)[0]
         details = request_movie_details(id)
-        output_file.write(details + "\n")
+
+        output_file.write(process(details))
         counter += 1
 print(
-    f">>> Finishing movie population, requested {counter} lines in {time.time() - start_time:.0f} s")
+    f'>> Finishing movie population, requested {counter} lines in {time.time() - start_time:.0f} s')
 
 
 if len(failed_requests) > 0:
-    print("x Failed to retrieve the details of the following movies:")
+    print('x Failed to retrieve the details of the following movies:')
     print(failed_requests)
 
 
-output_file.close()
-
-# zip content
-
-print("> Closing TMDB crawler")
+print('> Closing TMDB crawler')
